bcbcd578ba025a54c61cb0921e8eaeec0c3e91d1
Alin <alinalin03030@gmail.com>
2025-04-09 02:56:41 +0000

    my todo app

---
+++ src/lib/store.ts
@@ -0,0 +1,33 @@
+// src/lib/store.ts
+import { create } from 'zustand';
+
+type Todo = {
+  id: string;
+  text: string;
+  done: boolean;
+};
+
+type TodoStore = {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  toggleTodo: (id: string) => void;
+  deleteTodo: (id: string) => void;
+};
+
+export const useTodoStore = create<TodoStore>((set) => ({
+  todos: [],
+  addTodo: (text) =>
+    set((state) => ({
+      todos: [...state.todos, { id: Date.now().toString(), text, done: false }],
+    })),
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      ),
+    })),
+  deleteTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+}));
